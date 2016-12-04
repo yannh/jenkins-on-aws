408bdc114356e40bbbef90023ee01c89153e6456
Yann Hamon <yann.hamon@9cookies.com>
2016-12-04 11:36:50 +0100

    Add travis file for syntax validation / linting

--- puppet/site/profiles/manifests/jenkins_master.pp
+++ puppet/site/profiles/manifests/jenkins_master.pp
@@ -1,20 +1,20 @@
 class profiles::jenkins_master {
   class {
-    "jenkins":
+    'jenkins':
       repo            => false,
-      direct_download => "https://pkg.jenkins.io/debian-stable/binary/jenkins_2.19.4_all.deb",
+      direct_download => 'https://pkg.jenkins.io/debian-stable/binary/jenkins_2.19.4_all.deb',
       install_java    => false,
-      require         => [Package["daemon"], Class["java"]];
+      require         => [Package['daemon'], Class['java']];
 
   }
 
   file {
     '/usr/local/bin/snapshot-ebs.sh':
       ensure  => present,
-      mode    => "0755",
-      owner   => "root",
-      group   => "root",
-      content => template("profiles/snapshot-ebs.sh");
+      mode    => '0755',
+      owner   => 'root',
+      group   => 'root',
+      content => template('profiles/snapshot-ebs.sh');
   }
 
   cron {
